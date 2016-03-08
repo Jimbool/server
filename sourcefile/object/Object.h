452952cebb93ca771393d0d59b21158173fb8cf9
Jimbo <jimboo.lu@gmail.com>
2016-03-08 21:12:45 +0800

    upload

--- sourcefile/object/Object.h
+++ sourcefile/object/Object.h
@@ -1,13 +1,12 @@
-#pragma once
 #include <string>
 using namespace std;
 class Object
 {
 public:
-	string *Name;
 	Object();
-	~Object();
+    virtual	~Object();
 	virtual bool Equals(Object *object);
 	virtual bool Equals(Object &object);
+	virtual Object Clone();
 };
 
